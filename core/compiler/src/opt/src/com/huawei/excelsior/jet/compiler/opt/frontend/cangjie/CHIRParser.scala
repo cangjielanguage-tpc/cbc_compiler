9729056dac976d2a86293d77b5cd52b48ff2ff80
Ivan Trepakov <trepakov.ivan@huawei.com>
2026-08-22 12:53:36 +0700

    fixup! cbc: support

--- core/compiler/src/opt/src/com/huawei/excelsior/jet/compiler/opt/frontend/cangjie/CHIRParser.scala
+++ core/compiler/src/opt/src/com/huawei/excelsior/jet/compiler/opt/frontend/cangjie/CHIRParser.scala
@@ -2094,7 +2094,8 @@ trait CHIRParser
 
             case t: SignatureType.Box =>
               // Variable-sized type
-              New(SignatureType.Box(retType))()
+              val box = SignatureType.Box(retType)
+              NewGeneric(box)(loadTypeInfo(box))
 
             case SignatureType.Address =>
               // Type variable
