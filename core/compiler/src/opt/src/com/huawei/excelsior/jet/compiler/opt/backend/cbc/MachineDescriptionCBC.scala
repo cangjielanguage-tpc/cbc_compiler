0f4088fc6d4bd5ff2e05b923fc810879297e57dd
Denis Enes <enes.denis1@huawei.com>
2026-09-01 18:54:22 +0700

    fix: GetStaticFieldSeqRef

--- core/compiler/src/opt/src/com/huawei/excelsior/jet/compiler/opt/backend/cbc/MachineDescriptionCBC.scala
+++ core/compiler/src/opt/src/com/huawei/excelsior/jet/compiler/opt/backend/cbc/MachineDescriptionCBC.scala
@@ -91,6 +91,7 @@ trait MachineDescriptionCBC extends MachineDescription { self: Universe with Bac
 
       case Edge(_: HasFrameSlot, _: Box) => true
 
+      case Edge(_: DerivedPtr.BaseHandle, _: GetStaticFieldSeqRef) => false
       case Edge(_: DerivedPtr.BaseHandle, _: FieldSeqOperation) => true
 
       case _ => super.shouldBeUsedAsImmediate(use)
@@ -251,7 +252,7 @@ trait MachineDescriptionCBC extends MachineDescription { self: Universe with Bac
       | SpawnFuture | SpawnClosure
       | OptionTagGeneric | OptionPayloadGeneric | NewNoneOptionGeneric | NewSomeOptionGeneric | SaveCallRefTypeInfo
       | AssignGeneric | InstanceOfGeneric | NewGeneric
-      | AtomicOps.AtomicNode) => true
+      | AtomicOps.AtomicNode | DerivedPtr.Local | DerivedPtr.Global) => true
 
     case _: (TypeTest | CallTarget | MutFuncArgNode | RecordArrayGet) => true // always grouped with another node
 
