0aa435e50abe58359683b9b640a5b9f580a03c07
Ivan Trepakov <trepakov.ivan@huawei.com>
2026-09-10 16:29:37 +0700

    chir: exclude $instantiated$ methods from duplicate checks

--- core/compiler/src/o2-lib/src/com/huawei/excelsior/jet/compiler/o2lib/fe/CHIRSymLevelBuilderImpl.scala
+++ core/compiler/src/o2-lib/src/com/huawei/excelsior/jet/compiler/o2lib/fe/CHIRSymLevelBuilderImpl.scala
@@ -186,7 +186,7 @@ class CHIRSymLevelBuilderImpl extends CHIRSymLevelBuilder {
       }
 
       if (exportedName != null) {
-        val dups = clazz.getDeclaredMethods.filter(_.getExportedName == XString(exportedName)).toSeq
+        val dups = clazz.getDeclaredMethods.filter(f => f.getExportedName == XString(exportedName) && !f.getName.contains("$instantiated$")).toSeq
         assert(dups.isEmpty, s"Unexpected duplicate linkage names: ${methodByO2Object(m) +: dups}")
         m.markAsExported(XString(exportedName))
       }
